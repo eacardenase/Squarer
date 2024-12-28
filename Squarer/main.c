6cae1d947bc5afabf49820e74f96329ac2368574
Edwin Cardenas <eacardenase@gmail.com>
2024-12-28 14:43:51 -0500

    Initial Commit

---
+++ Squarer/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  Squarer
+//
+//  Created by Edwin Cardenas on 12/28/24.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
